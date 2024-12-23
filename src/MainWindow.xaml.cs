3a617f1cf9a1b4a54ced14dca756a2e000dd3ab7
Gustavo Carvalho <signoregustavo@gmail.com>
2024-12-23 10:59:54 -0300

    Update MainWindow.xaml.cs

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace CanaryLauncherUpdate
 {
 	public partial class MainWindow : Window
 	{
-		static string launcerConfigUrl = "https://github.com/siignore/canary-launcher/blob/master/launcher_config.json";
+		static string launcerConfigUrl = "https://github.com/siignore/canary-launcher/blob/main/launcher_config.json";
 		// Load informations of launcher_config.json file
 		static ClientConfig clientConfig = ClientConfig.loadFromFile(launcerConfigUrl);
 
